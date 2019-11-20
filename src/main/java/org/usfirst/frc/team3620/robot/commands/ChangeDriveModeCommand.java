2b35a2bb387baf43dad7ff35d61fb72ec643c448
Nick Zimanski <nick.zimanski@gmail.com>
2019-11-19 19:50:41 -0500

    Field and display read + telemetry improvements

--- src/main/java/org/usfirst/frc/team3620/robot/commands/ChangeDriveModeCommand.java
+++ src/main/java/org/usfirst/frc/team3620/robot/commands/ChangeDriveModeCommand.java
@@ -8,7 +8,9 @@ import edu.wpi.first.wpilibj.command.Command;
 
 public class ChangeDriveModeCommand extends Command {
 
+    /**The robot's mode before it enters firing mode. Defaults to arcade */
     private RobotDriveMode prevMode = RobotDriveMode.ARCADE;
+    /**Whether or not the bot is issued to change to or from fire mode */
     private boolean fireMode;
 
     public ChangeDriveModeCommand(boolean fireMode) {
@@ -17,20 +19,24 @@ public class ChangeDriveModeCommand extends Command {
 
     // Called just before this Command runs the first time
     protected void initialize() {
-        if (fireMode && Robot.driveMode != RobotDriveMode.FIRING) Robot.driveMode = RobotDriveMode.FIRING;
+        //Enters the robot into firing mode
+        if (fireMode && Robot.driveMode != RobotDriveMode.FIRING) {
+            Robot.driveMode = RobotDriveMode.FIRING;
+            Robot.fireModeSubsystem.activate();
+        }
+
+        //Shuts the robot out of firing mode
         else if (fireMode && Robot.driveMode == RobotDriveMode.FIRING) Robot.driveMode = prevMode;
-        System.out.println("here");
+
+        else if (!fireMode && Robot.driveMode == RobotDriveMode.FIRING) {return;}
         
+        //Switches between arcade and tank drive
         if (Robot.driveMode != RobotDriveMode.FIRING) {
                 if (Robot.driveMode == RobotDriveMode.ARCADE) {Robot.driveMode = RobotDriveMode.TANK;}
                 else if (Robot.driveMode == RobotDriveMode.TANK) {Robot.driveMode = RobotDriveMode.ARCADE;}
                 prevMode = Robot.driveMode;
-                System.out.println("Activating drive mode: " + Robot.driveMode);
         }
-        else {
-            Robot.fireModeSubsystem.activate();
-            System.out.println("Activating fire mode: " + Robot.driveMode);
-        }
+        System.out.println("Activating drive mode: " + Robot.driveMode);
     }
 
     // Called repeatedly when this Command is scheduled to run
--- src/main/java/org/usfirst/frc/team3620/robot/commands/FireModeCommand.java
+++ src/main/java/org/usfirst/frc/team3620/robot/commands/FireModeCommand.java
@@ -21,11 +21,18 @@ public class FireModeCommand extends Command {
 
     private RequestShotCommand shot;
 
-    private String[] CORRECT_SEQUENCE = {"up", "up", "down", "down", "left", "right", "left", "right", "X", "O"};
-    private List<String> patternSequence = new ArrayList<String>();
-    private boolean patternActive = false;
+    /**Whether or not the pad is accepting firing inputs */
+    private boolean firing = false;
+    /**Timer tracking the window to enter firing inputs */
     private Timer time = new Timer();
-    private int firingBarrel = 0; //0 is left, 1 is right
+    /**Amount of time, in seconds, that the firing window is open */
+    private double firingTime = 6.0;
+    /**0 is left, 1 is right*/
+    private int barrel = 0;
+    /**Number of buttons pressed in firing sequence*/
+    private int numPressed = 0;
+    /**Required number of presses to fire*/
+    final int MAX_PRESSES = 16;
     
     public FireModeCommand(Joystick joystick) {
         this.joystick = joystick;
@@ -40,70 +47,74 @@ public class FireModeCommand extends Command {
         shot = new RequestShotCommand();
     }
 
-    void startFirePattern(int barrel) {
-        firingBarrel = barrel;
-        patternSequence.clear();
-        patternActive = true;
+    /** Opens the window for entering firing inputs
+     * @param barrel Which barrel the bot is firing from. 0 is left, 1 is right*/
+    void startFire(int barrel) {
+        this.barrel = barrel;
+        numPressed = 0;
+        firing = true;
         time.start();
-        System.out.println("Starting firing pattern!");
+        System.out.println("Starting firing pattern! Press start or select to interrupt.");
     }
 
-    void endFirePattern() {
-        if (patternActive) {
-            shot.cancel();
-            fill0.cancel();
-            fill1.cancel();
-            patternSequence.clear();
-            patternActive = false;
-            time.stop();
-            time.reset();
-            System.out.println("Firing pattern ended!");
-        }
-    }
+    /**Checks the entered inputs for having reached the maximum, then fires and ends firing mode
+     * @return Whether or not to fire the barrel
+     */
+    boolean checkFire() {
+        if (time.get() < firingTime) {
+            if (joystick.getRawButtonReleased(1)) {numPressed++; System.out.println("Buttons pressed in current sequence: " + numPressed);}
+            if (joystick.getRawButtonReleased(2)) {numPressed++; System.out.println("Buttons pressed in current sequence: " + numPressed);}
+            if (joystick.getRawButtonReleased(3)) {numPressed++; System.out.println("Buttons pressed in current sequence: " + numPressed);}
+            if (joystick.getRawButtonReleased(4)) {numPressed++; System.out.println("Buttons pressed in current sequence: " + numPressed);}
+            if (joystick.getRawButtonReleased(5)) {numPressed++; System.out.println("Buttons pressed in current sequence: " + numPressed);}
+            if (joystick.getRawButtonReleased(6)) {numPressed++; System.out.println("Buttons pressed in current sequence: " + numPressed);}
+            if (joystick.getRawButtonReleased(7)) {numPressed++; System.out.println("Buttons pressed in current sequence: " + numPressed);}
+            if (joystick.getRawButtonReleased(8)) {numPressed++; System.out.println("Buttons pressed in current sequence: " + numPressed);}
 
-    void checkFirePattern() {
-        if (patternActive && time.get() <= 10.0) {
-        //check for up up down down left right left right X O start
-            if (patternSequence.size() == 10) {
-                for (int i = 0; i< 10; i++) {
-                    if (patternSequence.remove(0).equals(CORRECT_SEQUENCE[i])) continue;
-                    else {
-                        endFirePattern();
-                        return;
-                    }
-                }
+            if (joystick.getRawButtonReleased(9)) {System.out.println("Interrupting firing sequence... Exiting to fire mode"); endFire();}
+            if (joystick.getRawButtonReleased(10)) {System.out.println("Interrupting firing sequence... Exiting to previous drive mode"); end();}
 
-                if (firingBarrel == 1) fill1.start();
-                else fill0.start();
-                endFirePattern();
+            if (numPressed >= MAX_PRESSES) {
+                endFire();
+                return true;
             }
         }
-        else endFirePattern();
+        else {
+            System.out.println("Expiring firing sequence...");
+            endFire();
+        }
+        return false;
+    }
+
+    /**Closes the window for enter firing inputs */
+    void endFire() {
+        time.stop();
+        time.reset();
+        numPressed = 0;
+        if (firing) System.out.println("Firing sequence ended!");
+        firing = false;
+    }
+
+    /**Fires the selected barrel */
+    void fire() {
+        if (barrel == 0) fill0.start();
+        else if (barrel == 1) fill1.start();
+        System.out.println("Firing barrel " + barrel);
     }
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
-        if (!patternActive) {
+        if (!firing) {
             if (joystick.getRawButton(3)) lidUp.start(); //up
             else lidUp.cancel();
             if (joystick.getRawButton(2)) lidDown.start(); //down
             else lidDown.cancel();
 
-            if (joystick.getRawButton(4)) startFirePattern(1); //right
-            if (joystick.getRawButton(1)) startFirePattern(0); //left
-
+            if (joystick.getRawButton(4)) startFire(1); //right
+            if (joystick.getRawButton(1)) startFire(0); //left
         }
         else {
-
-            if (joystick.getRawButtonReleased(3)) patternSequence.add("up");
-            else if (joystick.getRawButtonReleased(2)) patternSequence.add("down");
-            else if (joystick.getRawButtonReleased(4)) patternSequence.add("right");
-            else if (joystick.getRawButtonReleased(1)) patternSequence.add("left");
-            else if (joystick.getRawButtonReleased(7)) patternSequence.add("X");
-            else if (joystick.getRawButtonReleased(8)) patternSequence.add("O");
-            else if (joystick.getRawButtonReleased(5) || joystick.getRawButtonReleased(6) || joystick.getRawButtonReleased(9) || joystick.getRawButtonReleased(10)) endFirePattern();
-
-            checkFirePattern();
+            if (checkFire()) fire();
         }
     }
 
@@ -115,6 +126,7 @@ public class FireModeCommand extends Command {
 
     // Called once after isFinished returns true
     protected void end() {
+        endFire();
     }
 
     // Called when another command which requires one or more of the same
